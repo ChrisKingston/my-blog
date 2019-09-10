cd024dac64006e708350b571de70ce6d2ef6357a
Chris Kingston <chriskingston77@gmail.com>
2019-09-10 02:47:23 -0400

    first commit

---
+++ src/build/precache-manifest.0125eb0edf65d0e1e751be0d821e8d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd12c6f24bfa2d5fa9a0bab43ae95700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761189f795cb003fa9c2",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "623b6645468bf88ac080",
+    "url": "/static/js/2.99e789c6.chunk.js"
+  },
+  {
+    "revision": "761189f795cb003fa9c2",
+    "url": "/static/js/main.05b34523.chunk.js"
+  },
+  {
+    "revision": "eeab855a9420d0df9e27",
+    "url": "/static/js/runtime~main.a3235f1f.js"
+  }
+]);
